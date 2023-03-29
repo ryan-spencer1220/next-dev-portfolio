fdf2f074b9407f88c7a9127d247738fc8f58b255
Ryan Spencer <ryan.spencer1220@gmail.com>
2023-03-29 15:23:07 -0700

    add blog preview section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Headshot from "../public/headshot.jpg";
+import Project from "../public/example.png";
 import OutlinedButton from "./components/OutlinedButton";
 import SolidButton from "./components/SolidButton";
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
@@ -19,8 +20,7 @@ export default function Home() {
             </RoughNotation>
             , a developer based in Portland, Oregon.
           </h1>
-
-          <div>
+          <div className="text-slate-600">
             <p className="py-4">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Similique, pariatur, corrupti molestias suscipit provident rerum
@@ -49,12 +49,104 @@ export default function Home() {
           className="w-1/3 h-1/3 rounded-3xl shadow-lg shadow-gray-400"
         />
       </section>
-      <section className="flex">
+      <section className="pb-20">
         <div>
           <h2 className="text-4xl">Projects</h2>
-          <p className="text-xl text-slate-700 py-4">
+          <p className="text-xl text-slate-700 pt-4 pb-20">
             Some of the side projects I've been working on:
           </p>
+        </div>
+        <div className="grid grid-cols-2 gap-20">
+          <div>
+            <Image
+              src={Project}
+              alt="Sample Project Mockup"
+              className="rounded-3xl shadow-md shadow-gray-400"
+            />
+            <div className="py-10">
+              <h5 className="text-2xl font-bold">Project Name #1</h5>
+              <p className="text-slate-600 py-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A enim
+                illo laboriosam cum iste dicta quia culpa sed exercitationem
+                quas!
+              </p>
+              <p className="text-slate-600 font-bold">View Project</p>
+            </div>
+          </div>
+          <div>
+            <Image
+              src={Project}
+              alt="Sample Project Mockup"
+              className="rounded-3xl shadow-md shadow-gray-400"
+            />
+            <div className="py-10">
+              <h5 className="text-2xl font-bold">Project Name #2</h5>
+              <p className="text-slate-600 py-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A enim
+                illo laboriosam cum iste dicta quia culpa sed exercitationem
+                quas!
+              </p>
+              <p className="text-slate-600 font-bold">View Project</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2 className="text-4xl">Posts</h2>
+          <p className="text-xl text-slate-700 pt-4 pb-20">
+            Thoughts on what I'm building and learning.
+          </p>
+        </div>
+        <div className="grid grid-cols-3 gap-10">
+          <div>
+            <Image
+              src={Project}
+              alt="Sample Project Mockup"
+              className="rounded-3xl shadow-md shadow-gray-400"
+            />
+            <div className="py-10">
+              <h5 className="text-2xl font-bold">Project Name #1</h5>
+              <p className="text-slate-600 py-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A enim
+                illo laboriosam cum iste dicta quia culpa sed exercitationem
+                quas...
+              </p>
+              <p className="text-slate-600 font-bold">Read More</p>
+            </div>
+          </div>
+          <div>
+            <Image
+              src={Project}
+              alt="Sample Project Mockup"
+              className="rounded-3xl shadow-md shadow-gray-400"
+            />
+            <div className="py-10">
+              <h5 className="text-2xl font-bold">Blog Post #2</h5>
+              <p className="text-slate-600 py-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A enim
+                illo laboriosam cum iste dicta quia culpa sed exercitationem
+                quas..
+              </p>
+              <p className="text-slate-600 font-bold">Read More</p>
+            </div>
+          </div>
+          <div>
+            <Image
+              src={Project}
+              alt="Sample Project Mockup"
+              className="rounded-3xl shadow-md shadow-gray-400"
+            />
+            <div className="py-10">
+              <h5 className="text-2xl font-bold">Blog Post #3</h5>
+              <p className="text-slate-600 py-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A enim
+                illo laboriosam cum iste dicta quia culpa sed exercitationem
+                quas..
+              </p>
+              <p className="text-slate-600 font-bold">Read More</p>
+            </div>
+          </div>
         </div>
       </section>
     </>
