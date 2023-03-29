784a8e22101431ec32fd5a9ba109add4101afd21
Ryan Spencer <ryan.spencer1220@gmail.com>
2023-03-29 09:19:02 -0700

    add conditional navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,34 @@
+"use client";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const currentPath = usePathname();
+
   return (
     <nav>
-      <ul className="flex flex-row text-3xl justify-between">
+      <ul className="flex flex-row text-2xl justify-between">
         <li>RYAN SPENCER</li>
         <div className="flex flex-row justify-evenly">
-          <li className="px-8">About</li>
-          <li className="px-8">Blog</li>
-          <li className="px-8">
-            <FaGithub />
-          </li>
-          <li className="px-8">
-            <FaLinkedin />
-          </li>
+          <li className="px-6">About</li>
+          <li className="px-6">Projects</li>
+          {currentPath === "/" && (
+            <>
+              <li className="px-6">Blog</li>
+              <li className="px-6">Contact</li>
+            </>
+          )}
+          {currentPath !== "/" && (
+            <>
+              <li className="px-6">
+                <FaGithub />
+              </li>
+              <li className="px-6">
+                <FaLinkedin />
+              </li>
+            </>
+          )}
         </div>
       </ul>
     </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
+"use client";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Headshot from "../public/headshot.jpg";
 import OutlinedButton from "./components/OutlinedButton";
 import SolidButton from "./components/SolidButton";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <section className="flex py-40">
+      <section className="flex pt-40 pb-20">
         <div className="pr-40">
           <h1 className="text-5xl">
-            Hello! I'm Ryan, a developer based in Portland, Oregon.
+            Hello! I'm{" "}
+            <RoughNotation type="highlight" show={true} color="yellow">
+              Ryan Spencer
+            </RoughNotation>
+            , a developer based in Portland, Oregon.
           </h1>
+
           <div>
             <p className="py-4">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -41,6 +48,14 @@ export default function Home() {
           alt="Ryan Photo"
           className="w-1/3 h-1/3 rounded-3xl shadow-lg shadow-gray-400"
         />
+      </section>
+      <section className="flex">
+        <div>
+          <h2 className="text-4xl">Projects</h2>
+          <p className="text-xl text-slate-700 py-4">
+            Some of the side projects I've been working on:
+          </p>
+        </div>
       </section>
     </>
   );
